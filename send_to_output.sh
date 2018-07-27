0a3bb53a6c9081f2f22a3389d386d0e9cd87ffd3
Byron Katz <byron@renomad.com>
2018-07-27 14:19:50 -0400

    slight improvements

--- send_to_output.sh
+++ send_to_output.sh
@@ -3,6 +3,11 @@
 rm -fr output
 ./ssi_expander.py output
 
+# delete certain files from output directory
+rm output/ssi*
+rm output/readme.md
+rm output/send_to_output.sh
+
 # delete the contents of the /var/www/html directory
 sudo rm -fr /var/www/html/*
 
